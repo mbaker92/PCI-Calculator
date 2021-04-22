60ce8ea0175ffe343566dcb341bd98929b76d2cf
Matthew Baker <Hackmattr@gmail.com>
2021-04-22 10:55:51 -0700

    Requirements.txt. ACP Constructor

--- PCICalculator/PCICalculator.py
+++ PCICalculator/PCICalculator.py
@@ -36,15 +36,9 @@ def ImportRawDataCSV(name):
         # For loop to get each sample as a ACPRaw object and store each object in the RawDataList
         for row in reader:
             if 'StIDSecID' not in row[0]:   # Skip the Header in the file
-                 RawDataList.append(ACPRaw(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16],
-                                           row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28], row[29], row[30], row[31], row[32], row[33],
-                                           row[34], row[35], row[36], row[37]))
-
-
-    # Function used to create the Output CSV file with PCI calculations
+                RawDataList.append(ACPRaw(*row[:38]))
+             
 def OutputCSV():
-
-    # Open up the save file using the imported file's directory and filename with PCI appended to the end. 
     with open(os.path.normpath(os.path.join(fileDirect,fileName +' PCI.csv')),'w',newline='') as csvFileOut:
         writer=csv.writer(csvFileOut)   # Create the file writer
 
